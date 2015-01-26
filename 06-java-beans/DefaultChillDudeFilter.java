7bacf418a15dc961b4af443815755aaf00157208
Brandon Zeeb <phasebash@gmail.com>
2015-01-26 14:53:59 -0500

    adding doco

--- 06-java-beans/DefaultChillDudeFilter.java
+++ 06-java-beans/DefaultChillDudeFilter.java
@@ -2,6 +2,14 @@ import java.util.List;
 import java.util.Map;
 
 public class DefaultChillDudeFilter implements ChillDudeFilter {
+
+    /**
+     * Please use Stream API. 
+     * The .max method might be useful.
+     *
+     * @param dudes
+     * @return
+     */
     @Override
     public String oldestDude(List<ChillDude> dudes) {
         return null;
