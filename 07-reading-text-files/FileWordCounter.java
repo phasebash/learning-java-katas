6caefd88a1665961276ad16ee4af78051b9abed0
Brandon Zeeb <phasebash@gmail.com>
2015-01-26 15:09:37 -0500

    fixing defect.

--- 07-reading-text-files/FileWordCounter.java
+++ 07-reading-text-files/FileWordCounter.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class FileWordCounter {
     
-    public Map<String, Integer> countWords(String fileName) {
+    public Map<String, Long> countWords(String fileName) {
         return Collections.emptyMap();
     }
     
